6a97feddd11c8c5621bd7e3ded867a51ba565119
honnage <hon_da-1998@hotmail.com>
2024-12-01 23:00:15 +0700

    Update .gitignore to exclude node_modules

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const morgan = require('morgan')
+const cors = require('cors')
 
 
 
 // Middleware
 app.use(morgan('dev'))
-
+app.use(express.json({ limit: '20mb' }))
+app.use(cors())
 
 
 
