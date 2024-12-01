0d042b1b92e79cc56005ddc1290e3d4d4093f447
honnage <hon_da-1998@hotmail.com>
2024-12-01 23:24:56 +0700

    readdirSync routes

--- server.js
+++ server.js
@@ -3,18 +3,14 @@ const app = express()
 const port = 3000
 const morgan = require('morgan')
 const cors = require('cors')
-
-const authRouter = require('./routes/auth')
-const categoryRouter = require('./routes/category')
+const { readdirSync  } = require('fs')
 
 // Middleware
 app.use(morgan('dev'))
 app.use(express.json({ limit: '20mb' }))
 app.use(cors())
 
-
-app.use('/api', authRouter)
-app.use('/api', categoryRouter)
+readdirSync('./routes').map((path) => app.use('/api', require('./routes/'+path)))
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
