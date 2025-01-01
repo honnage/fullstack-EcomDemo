4360d70989a95484438e897bc413972aebdab995
honnage <hon_da-1998@hotmail.com>
2025-01-01 22:49:09 +0700

    API register and login

--- Server/controllers/auth.js
+++ Server/controllers/auth.js
@@ -1,7 +1,11 @@
+const prisma = require('../config/prisma') 
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 exports.register = async(req, res) => {
     try {
         const {email, password} = req.body
-        console.log(email, password)
+
         // Validate body
         if (!email){
             return res.status(400).json({message: 'Email is required!!!'})
@@ -10,8 +14,28 @@ exports.register = async(req, res) => {
             return res.status(400).json({message: 'Password is required!!!'})
         }
 
+        // Check Email in DB already?
+        const user = await prisma.user.findFirst({
+            where:{
+                email: email
+            }
+        })
 
-        res.send('Hellow Register In Controller')
+        if (user){
+            return res.status(400).json({message: "Email already exits!!"})
+        }
+
+        // Hash Password
+        const hashPassword = await bcrypt.hash(password, 10)
+        // console.log(email, user, password, hashPassword)
+
+        await prisma.user.create({
+            data: {
+                email: email,
+                password: hashPassword
+            }
+        })
+        res.send('Register Seccess')
 
     } catch (err) {
         console.log(err)
@@ -21,7 +45,40 @@ exports.register = async(req, res) => {
 
 exports.login = async(req, res) => {
     try {
-        res.send('Hellow Login In Controller')
+        const {email, password} = req.body
+        console.log(email, password)
+
+        // Check Email
+        const user = await prisma.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+
+        if (!user || !user.enabled){
+            return res.status(400).json({message: "User not found or not enabled"})
+        }
+
+        // Check Password
+        const isMatchPassword = await bcrypt.compare(password, user.password)
+        if (!isMatchPassword){
+            return res.status(400).json({message: "Password invalid!!!"})
+        }
+
+        // Create Payload and Generate Token
+        const payload = {
+            id: user.id,
+            email: user.email,
+            role: user.role
+        }
+
+        // const secretKey = process.env.SECRET_KEY || 'systeam_password';
+        jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: "1h" }, (err, token) => {
+            if (err) {
+                return res.status(500).json({ message: "Server Error" })
+            }
+            res.status(200).json({ payload, token })
+        })
 
     } catch (err) {
         console.log(err)
