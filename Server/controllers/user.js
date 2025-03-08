d9937249ad0a018ee3a5cfa5a4ff461baa5caa1e
honnage <hon_da-1998@hotmail.com>
2025-03-09 02:21:01 +0700

    routes and controller user

--- Server/controllers/user.js
+++ Server/controllers/user.js
@@ -6,3 +6,75 @@ exports.listUsers = async(req, res) => {
         res.status(500).json({message: 'Server Error'})
     }
 }
+
+exports.changeStatus = async(req, res) => {
+    try{
+        res.send('Hello changeStatus')
+    } catch(err){
+        console.log(err)
+        res.status(500).json({message: 'Server Error'})
+    }
+}
+
+exports.changeRole = async(req, res) => {
+    try{
+        res.send('Hello changeRole')
+    } catch(err){
+        console.log(err)
+        res.status(500).json({message: 'Server Error'})
+    }
+}
+
+exports.userCart = async(req, res) => {
+    try{
+        res.send('Hello userCart')
+    } catch(err){
+        console.log(err)
+        res.status(500).json({message: 'Server Error'})
+    }
+}
+
+exports.getUserCart = async(req, res) => {
+    try{
+        res.send('Hello getUserCart')
+    } catch(err){
+        console.log(err)
+        res.status(500).json({message: 'Server Error'})
+    }
+}
+
+exports.emptyCart = async(req, res) => {
+    try{
+        res.send('Hello emptyCart')
+    } catch(err){
+        console.log(err)
+        res.status(500).json({message: 'Server Error'})
+    }
+}
+
+exports.saveAddress = async(req, res) => {
+    try{
+        res.send('Hello saveAddress')
+    } catch(err){
+        console.log(err)
+        res.status(500).json({message: 'Server Error'})
+    }
+}
+
+exports.saveOrder = async(req, res) => {
+    try{
+        res.send('Hello saveOrder')
+    } catch(err){
+        console.log(err)
+        res.status(500).json({message: 'Server Error'})
+    }
+}
+
+exports.getOrder = async(req, res) => {
+    try{
+        res.send('Hello getOrder')
+    } catch(err){
+        console.log(err)
+        res.status(500).json({message: 'Server Error'})
+    }
+}
--- Server/routes/user.js
+++ Server/routes/user.js
@@ -1,18 +1,28 @@
 const express = require('express')
 const router = express.Router()
 const { authCheck, adminCheck } = require('../middlewares/authCheck')
-const {listUsers} = require('../controllers/user')
+const {
+    listUsers,
+    changeStatus,
+    changeRole,
+    userCart,
+    getUserCart,
+    emptyCart,
+    saveAddress,
+    saveOrder,
+    getOrder
+} = require('../controllers/user')
 
 router.get('/users', authCheck, adminCheck, listUsers)
-router.post('/change-status')
-router.post('/change-role')
+router.post('/change-status', authCheck, adminCheck, changeStatus)
+router.post('/change-role', authCheck, adminCheck, changeRole)
 
-router.post('/user/cart')
-router.get('/user/cart')
-router.delete('/user/cart')
+router.post('/user/cart', authCheck, userCart)
+router.get('/user/cart', authCheck, getUserCart)
+router.delete('/user/cart', authCheck, emptyCart)
 
-router.post('/user/address')
-router.post('/user/order')
-router.get('/user/order')
+router.post('/user/address', authCheck, saveAddress)
+router.post('/user/order',  authCheck, saveOrder)
+router.get('/user/order',  authCheck, getOrder)
 
 module.exports = router
