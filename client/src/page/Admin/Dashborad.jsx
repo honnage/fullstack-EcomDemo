5328222e0afb3dbcbd03be73140889d874487a95
honnage <hon_da-1998@hotmail.com>
2025-03-15 16:16:15 +0700

    update layout

---
+++ client/src/page/Admin/Dashborad.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashborad = () => {
+  return (
+    <div>
+      Dashborad
+    </div>
+  )
+}
+
+export default Dashborad
--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from '../page/Home'
+import Shop from '../page/shop'
+import Cart from '../page/Cart'
+import History from '../page/History'
+import Checkout from '../page/Checkout'
+import Login from '../page/Auth/Login'
+import Register from '../page/Auth/Register'
+import Layout from '../layouts/Layout'
+import LayoutAdmin from '../layouts/LayoutAdmins';
+import LayoutUser from '../layouts/Layoutuser'
+import Dashboard from '../page/Admin/Dashborad'
+import Category from '../page/Admin/Category'
+import Product from '../page/Admin/Product'
+import Manage from '../page/Admin/Manage'
+import HomeUser from '../page//User/HomeUser'
 
 const router = createBrowserRouter([
-    { path: '/', element: <h1>Home Page</h1>},
-    { path: 'shop', element: <h1>shop page</h1>},
-
+  { path: '/', 
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home />},
+      { path: 'shop', element:  <Shop />},
+      { path: 'cart', element:  <Cart />},
+      { path: 'history', element:  <History />},
+      { path: 'chekout', element:  <Checkout />},
+      { path: 'login', element:  <Login />},
+      { path: 'register', element:  <Register />},
+    ]
+  },
+  {
+    path: '/admin',
+    element: <LayoutAdmin />,
+    children: [
+      { index: true, element: <Dashboard />},
+      { path: 'category', element:  <Category />},
+      { path: 'product', element:  <Product />},
+      { path: 'manage', element:  <Manage />},
+    ]
+  },
+  {
+    path: '/user',
+    element: <LayoutUser/>,
+    children: [
+      { index: true, element: <HomeUser />},
+    ]
+  }
+  
 ])
 
 const AppRoutes = () => {
