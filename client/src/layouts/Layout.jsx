c600e43d85227043f92be93a8a6705ca4076779c
honnage <hon_da-1998@hotmail.com>
2025-03-15 18:21:14 +0700

    mainNav

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'
+import MainNav from '../components/MainNav'
 
 const Layout = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <MainNav />
+
       <hr/>
       <Outlet />
     </div>
